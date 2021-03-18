02755b0c42391314189ed2501545877c606354c6
LindaNab <l.nab@lumc.nl>
2021-03-18 22:46:27 +0100

    add additional results from hpc (run was cancelled due to time limit)

--- input/myscript.sh
+++ input/myscript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --job-name=simexvsmecor          # Job name
-#SBATCH --output=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen%a.out              # Output file name
-#SBATCH --error=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen%a.err               # Error file name
-#SBATCH --time=03:45:00                 # Time limit
+#SBATCH --output=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen%a_2.out              # Output file name
+#SBATCH --error=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen%a_2.err               # Error file name
+#SBATCH --time=04:00:00                 # Time limit
 #SBATCH --nodes=1                       # Number of nodes
 #SBATCH --ntasks-per-node=1             # MPI processes per node
 #SBATCH --array=1-14
@@ -14,4 +14,4 @@ scenario=${SLURM_ARRAY_TASK_ID}
 module purge
 module add statistical/R/4.0.2/gcc.8.3.1
 
-Rscript --vanilla ./input/simexvsmecor_slurmscript.R 5000 $scenario "./output/"
+Rscript --vanilla ./input/simexvsmecor_slurmscript.R 2000 $scenario "./output/"
