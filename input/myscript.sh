ba526882979589cc944de4ce6c70c33089857039
LindaNab <l.nab@lumc.nl>
2021-03-19 16:47:20 +0100

    add new output scen 7-14

--- input/myscript.sh
+++ input/myscript.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --job-name=simexvsmecor          # Job name
-#SBATCH --output=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen%a_2.out              # Output file name
-#SBATCH --error=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen%a_2.err               # Error file name
+#SBATCH --output=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen%a.out              # Output file name
+#SBATCH --error=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen%a.err               # Error file name
 #SBATCH --time=04:00:00                 # Time limit
 #SBATCH --nodes=1                       # Number of nodes
 #SBATCH --ntasks-per-node=1             # MPI processes per node
-#SBATCH --array=1-6
+#SBATCH --array=7-14
 
 # Read in scenario number
 scenario=${SLURM_ARRAY_TASK_ID} 
