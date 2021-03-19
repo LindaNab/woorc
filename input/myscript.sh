87c6843bc86bd884d325e15fb44019777d973383
LindaNab <l.nab@lumc.nl>
2021-03-19 12:02:07 +0100

    add new output hpc scen 1-6

--- input/myscript.sh
+++ input/myscript.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=04:00:00                 # Time limit
 #SBATCH --nodes=1                       # Number of nodes
 #SBATCH --ntasks-per-node=1             # MPI processes per node
-#SBATCH --array=1-14
+#SBATCH --array=1-6
 
 # Read in scenario number
 scenario=${SLURM_ARRAY_TASK_ID} 
@@ -14,4 +14,4 @@ scenario=${SLURM_ARRAY_TASK_ID}
 module purge
 module add statistical/R/4.0.2/gcc.8.3.1
 
-Rscript --vanilla ./input/simexvsmecor_slurmscript.R 2000 $scenario "./output/"
+Rscript --vanilla ./input/simexvsmecor_slurmscript.R 1000 $scenario "./output/"
