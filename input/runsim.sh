8c3999b8fe6e73bc5d73627adc0e90efa7f88249
Linda Nab <l.nab@lumc.nl>
2021-03-31 23:12:47 +0200

    run 4000 extra reps sim study

--- input/runsim.sh
+++ input/runsim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --job-name=simexvsmecor          # Job name
-#SBATCH --output=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen%a.out              # Output file name
-#SBATCH --error=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen%a.err               # Error file name
-#SBATCH --time=04:00:00                 # Time limit
+#SBATCH --output=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen_2%a.out              # Output file name
+#SBATCH --error=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen_2%a.err               # Error file name
+#SBATCH --time=10:00:00                 # Time limit
 #SBATCH --nodes=1                       # Number of nodes
 #SBATCH --ntasks-per-node=1             # MPI processes per node
 #SBATCH --array=1-18
@@ -14,4 +14,4 @@ scenario=${SLURM_ARRAY_TASK_ID}
 module purge
 module add statistical/R/4.0.2/gcc.8.3.1
 
-Rscript --vanilla ./input/output.R 1000 $scenario "./output/"
+Rscript --vanilla ./input/output.R 4000 $scenario "./output/"
