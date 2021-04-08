bbc229e508c2aadceadcac2e49afeed9de1e5854
Linda Nab <l.nab@lumc.nl>
2021-04-08 09:21:49 +0200

    change log and error message

--- input/runsim.sh
+++ input/runsim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=simexvsmecor          # Job name
-#SBATCH --output=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen_2%a.out              # Output file name
-#SBATCH --error=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen_2%a.err               # Error file name
+#SBATCH --output=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen_%a.out              # Output file name
+#SBATCH --error=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen_%a.err               # Error file name
 #SBATCH --time=10:00:00                 # Time limit
 #SBATCH --nodes=1                       # Number of nodes
 #SBATCH --ntasks-per-node=1             # MPI processes per node
