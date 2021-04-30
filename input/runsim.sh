cc17196074318bc1dfb1e4fe37510518d9f74cae
Linda Nab <l.nab@lumc.nl>
2021-04-30 15:52:59 +0200

    run scen no. 23

--- input/runsim.sh
+++ input/runsim.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=simexvsmecor          # Job name
 #SBATCH --output=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen_%a.out              # Output file name
 #SBATCH --error=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen_%a.err               # Error file name
-#SBATCH --time=10:00:00                 # Time limit
+#SBATCH --time=100:00:00                 # Time limit
 #SBATCH --nodes=1                       # Number of nodes
 #SBATCH --ntasks-per-node=1             # MPI processes per node
-#SBATCH --array=1-22
+#SBATCH --array=23
 
 # Read in scenario number
 scenario=${SLURM_ARRAY_TASK_ID}
