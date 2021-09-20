e25f2c05787ef1ea031bf467d36dfb488d55239c
Linda Nab <l.nab@lumc.nl>
2021-09-20 13:43:35 +0200

    run sim scennos 1-23 (except 13) 4000 extra runs

--- input/runsim_logreg.sh
+++ input/runsim_logreg.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=100:00:00                 # Time limit
 #SBATCH --nodes=1                       # Number of nodes
 #SBATCH --ntasks-per-node=1             # MPI processes per node
-#SBATCH --array=3-23
+#SBATCH --array=1,2,3,4,5,6,7,8,9,10,11,12,14,15,16,17,18,19,20,21,22,23
 
 # Read in scenario number
 scenario=${SLURM_ARRAY_TASK_ID}
