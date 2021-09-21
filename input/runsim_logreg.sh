9546c649bef8233117189dd7655c730b3604aa05
Linda Nab <l.nab@lumc.nl>
2021-09-21 08:34:00 +0200

    run scennos 1-12 and 14-23 3000 times (by mistake last time only 1000 times)

--- input/runsim_logreg.sh
+++ input/runsim_logreg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=simexvsmecor_logreg    # Job name
-#SBATCH --output=/exports/clinicalepi/Linda/simexvsmecor/job%A_logreg2_scen_%a.out              # Output file name
-#SBATCH --error=/exports/clinicalepi/Linda/simexvsmecor/job%A_logreg2_scen_%a.err               # Error file name
+#SBATCH --output=/exports/clinicalepi/Linda/simexvsmecor/job%A_logreg3_scen_%a.out              # Output file name
+#SBATCH --error=/exports/clinicalepi/Linda/simexvsmecor/job%A_logreg3_scen_%a.err               # Error file name
 #SBATCH --time=100:00:00                 # Time limit
 #SBATCH --nodes=1                       # Number of nodes
 #SBATCH --ntasks-per-node=1             # MPI processes per node
@@ -14,4 +14,4 @@ scenario=${SLURM_ARRAY_TASK_ID}
 module purge
 module add statistical/R/4.0.2/gcc.8.3.1
 
-Rscript --vanilla ./input/output_logreg.R 1000 $scenario "./output/"
+Rscript --vanilla ./input/output_logreg.R 3000 $scenario "./output/"
