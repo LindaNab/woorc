a44b84de0367c4841cfe063c0f461f523cb10e80
Linda Nab <l.nab@lumc.nl>
2021-09-29 07:56:49 +0200

    run 5000 times

--- input/runsim_logreg.sh
+++ input/runsim_logreg.sh
@@ -14,4 +14,4 @@ scenario=${SLURM_ARRAY_TASK_ID}
 module purge
 module add statistical/R/4.0.2/gcc.8.3.1
 
-Rscript --vanilla ./input/output_logreg.R 3000 $scenario "./output/"
+Rscript --vanilla ./input/output_logreg.R 5000 $scenario "./output/"
