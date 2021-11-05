5732295edef0326ae50b5b8375c5b6baf926d325
Linda Nab <l.nab@lumc.nl>
2021-11-05 23:47:55 +0100

    change number of reps

--- input/runsim.sh
+++ input/runsim.sh
@@ -14,4 +14,4 @@ scenario=${SLURM_ARRAY_TASK_ID}
 module purge
 module add statistical/R/4.0.2/gcc.8.3.1
 
-Rscript --vanilla ./input/output.R 1000 $scenario "./output/"
+Rscript --vanilla ./input/output.R 500 $scenario "./output/"
