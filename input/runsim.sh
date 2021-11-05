1019aa470e0afb07edc1bb90b2d4b59f7a6ccade
Linda Nab <l.nab@lumc.nl>
2021-11-05 23:03:48 +0100

    change names of folders

--- input/runsim.sh
+++ input/runsim.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=simexvsmecor          # Job name
-#SBATCH --output=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen_%a.out              # Output file name
-#SBATCH --error=/exports/clinicalepi/Linda/simexvsmecor/job%A_scen_%a.err               # Error file name
-#SBATCH --time=100:00:00                 # Time limit
+#SBATCH --job-name=woorc          # Job name
+#SBATCH --output=/exports/clinicalepi/Linda/woorc/job%A_scen_%a.out              # Output file name
+#SBATCH --error=/exports/clinicalepi/Linda/woorc/job%A_scen_%a.err               # Error file name
+#SBATCH --time=100:00:00                # Time limit
 #SBATCH --nodes=1                       # Number of nodes
 #SBATCH --ntasks-per-node=1             # MPI processes per node
-#SBATCH --array=15
+#SBATCH --array=1
 
 # Read in scenario number
 scenario=${SLURM_ARRAY_TASK_ID}
@@ -14,4 +14,4 @@ scenario=${SLURM_ARRAY_TASK_ID}
 module purge
 module add statistical/R/4.0.2/gcc.8.3.1
 
-Rscript --vanilla ./input/output.R 197 $scenario "./output/"
+Rscript --vanilla ./input/output.R 1000 $scenario "./output/"
