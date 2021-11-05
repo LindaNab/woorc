f4dbe34e4f91a4b3427357be2c342d1d41bdf0f1
Linda Nab <l.nab@lumc.nl>
2021-11-05 23:41:08 +0100

    fix error in calc of tau_sq

--- input/input.R
+++ input/input.R
@@ -24,7 +24,7 @@ nobs <- c(300, 150, 600, 1200)
 reliability <- c(0.99, 0.95, seq(from = 0.8, to = 0.1, by = -0.1), 0.05, 0.01)
 rel_nobs <- expand.grid(reliability = reliability, nobs = nobs)
 input[, c("nobs", "reliability")] <- rel_nobs[, c("nobs", "reliability")]
-input$tau_sq <- 0.25 / input$reliability
+input$tau_sq <- (0.25 / input$reliability) - 0.25
 
 ##############################
 # 2 - Add input to package ---
