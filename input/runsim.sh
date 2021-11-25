dccbbb97c0bddaa660c850be1b7ddfe84a09734e
Linda Nab <l.nab@lumc.nl>
2021-11-25 20:14:07 +0100

    run 4000 additional runs for sim type biasvsvar scennos 1-30

--- input/runsim.sh
+++ input/runsim.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=100:00:00                # Time limit
 #SBATCH --nodes=1                       # Number of nodes
 #SBATCH --ntasks-per-node=1             # MPI processes per node
-#SBATCH --array=1-65
+#SBATCH --array=1-30
 
 # Read in scenario number
 scenario=${SLURM_ARRAY_TASK_ID}
@@ -14,4 +14,4 @@ scenario=${SLURM_ARRAY_TASK_ID}
 module purge
 module add statistical/R/4.0.2/gcc.8.3.1
 
-Rscript --vanilla ./input/output.R 5000 "lowrel" $scenario "./output/"
+Rscript --vanilla ./input/output.R 4000 "biasvsvar" $scenario "./output/"
