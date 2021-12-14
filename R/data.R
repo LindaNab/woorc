7b172f0b4fe4b8912a7dcc9f462cc49c96ca272c
Linda Nab <l.nab@lumc.nl>
2021-12-14 14:11:34 +0100

    add summaries of simulation study results

--- R/data.R
+++ R/data.R
@@ -41,33 +41,67 @@
 #' @examples
 #' data("input_biasvsvar", package = "woorc")
 "input_biasvsvar"
-#' #' Summary of Simulation Study
-#' #'
-#' #' A dataset containing a summary of the results from the 48 scenarios x 3
-#' #' methods (= 144)
-#' #'
-#' #' @format A data frame with 144 rows and 19 variables:
-#' #' \describe{
-#' #'   \item{scen_no}{scenario no.}
-#' #'   \item{nobs}{number of observations (sample size)}
-#' #'   \item{tau_sq}{\eqn{\tau^2}}
-#' #'   \item{beta}{estimand}
-#' #'   \item{reliability}{Var(active_energy_expenditure) / Var(active_energy_expenditure_star)}
-#' #'   \item{bias}{bias}
-#' #'   \item{bias_mcse}{monte carlo standard error of the bias}
-#' #'   \item{mse}{mean squared error}
-#' #'   \item{mse_mcse}{monte carlo standard error of the mean squared error}
-#' #'   \item{cover}{coverage}
-#' #'   \item{cover_mcse}{monte carlo standard error of the coverage}
-#' #'   \item{modelse}{model based standard error}
-#' #'   \item{modelse_mcse}{monte carlo standard error of the model based standard error}
-#' #'   \item{empse}{empirical standard error}
-#' #'   \item{empse_mcse}{monte carlo standard error of the empirical standard error}
-#' #'   \item{nsim}{number of replications}
-#' #'   \item{nsim_mcse}{monte carlo standard error of the number of replications (NA)}
-#' #'   \item{r_sqaured_est}{estimated R-squared of Y ~ X + Z}
-#' #'   \item{perc_bias}{percentage bias}
-#' #' }
-#' #' @examples
-#' #' data("summary", package = "woorc")
-#' "summary"
+#' Summary of Simulation Study // Bias vs Variance Tradeoff RegCal
+#'
+#' A dataset containing a summary of the results from the 30 scenarios x 3
+#' methods (= 90)
+#'
+#' @format A data frame with 90 rows and 21 variables:
+#' \describe{
+#'   \item{scen_no}{scenario no.}
+#'   \item{method}{method used (uncor/mecor.delta/mecor.btstrp)}
+#'   \item{nobs}{number of observations (sample size)}
+#'   \item{tau_sq}{\eqn{\tau^2}}
+#'   \item{beta}{estimand}
+#'   \item{sigma_sq}{\eqn{\sigma^2}}
+#'   \item{reliability}{Var(active_energy_expenditure) / Var(active_energy_expenditure_star)}
+#'   \item{bias}{bias}
+#'   \item{bias_mcse}{monte carlo standard error of the bias}
+#'   \item{mse}{mean squared error}
+#'   \item{mse_mcse}{monte carlo standard error of the mean squared error}
+#'   \item{cover}{coverage}
+#'   \item{cover_mcse}{monte carlo standard error of the coverage}
+#'   \item{modelse}{model based standard error}
+#'   \item{modelse_mcse}{monte carlo standard error of the model based standard error}
+#'   \item{empse}{empirical standard error}
+#'   \item{empse_mcse}{monte carlo standard error of the empirical standard error}
+#'   \item{nsim}{number of replications}
+#'   \item{nsim_mcse}{monte carlo standard error of the number of replications (NA)}
+#'   \item{r_sqaured_est}{estimated R-squared of Y ~ X + Z}
+#'   \item{perc_bias}{percentage bias}
+#' }
+#' @examples
+#' data("summary_biasvsvar", package = "woorc")
+"summary_biasvsvar"
+#' Summary of Simulation Study // Performance RegCal when Reliability is Low
+#'
+#' A dataset containing a summary of the results from the 65 scenarios x 3
+#' methods (= 195)
+#'
+#' @format A data frame with 195 rows and 21 variables:
+#' \describe{
+#'   \item{scen_no}{scenario no.}
+#'   \item{method}{method used (uncor/mecor.delta/mecor.btstrp)}
+#'   \item{nobs}{number of observations (sample size)}
+#'   \item{tau_sq}{\eqn{\tau^2}}
+#'   \item{beta}{estimand}
+#'   \item{sigma_sq}{\eqn{\sigma^2}}
+#'   \item{reliability}{Var(active_energy_expenditure) / Var(active_energy_expenditure_star)}
+#'   \item{bias}{bias}
+#'   \item{bias_mcse}{monte carlo standard error of the bias}
+#'   \item{mse}{mean squared error}
+#'   \item{mse_mcse}{monte carlo standard error of the mean squared error}
+#'   \item{cover}{coverage}
+#'   \item{cover_mcse}{monte carlo standard error of the coverage}
+#'   \item{modelse}{model based standard error}
+#'   \item{modelse_mcse}{monte carlo standard error of the model based standard error}
+#'   \item{empse}{empirical standard error}
+#'   \item{empse_mcse}{monte carlo standard error of the empirical standard error}
+#'   \item{nsim}{number of replications}
+#'   \item{nsim_mcse}{monte carlo standard error of the number of replications (NA)}
+#'   \item{r_sqaured_est}{estimated R-squared of Y ~ X + Z}
+#'   \item{perc_bias}{percentage bias}
+#' }
+#' @examples
+#' data("summary_lowrel", package = "woorc")
+"summary_lowrel"
--- input/summary.R
+++ input/summary.R
@@ -10,14 +10,24 @@
 ##############################
 
 ##############################
-# 1 - Create data.frame summary
+# 1 - Create data.frame summary_biasvar
 ##############################
-process_output(1:48)
-summary <- summarise_sim(1:48,
-                         use_input = input,
-                         processed_dir = "./output/processed/")
+process_output(type = "biasvsvar",
+               scen_nos = 1:30)
+summary_biasvsvar <- summarise_sim(1:30,
+                                   use_input = input_biasvsvar,
+                                   processed_dir = "./output/processed/biasvsvar_")
+##############################
+# 2 - Create data.frame summary_lowrel
+##############################
+process_output(type = "lowrel",
+               scen_nos = 1:65)
+summary_lowrel <- summarise_sim(1:65,
+                                   use_input = input_lowrel,
+                                   processed_dir = "./output/processed/lowrel_")
 
 ##############################
 # 2 - Add summary to package ---
 ##############################
-usethis::use_data(summary, overwrite = TRUE)
+usethis::use_data(summary_biasvsvar, overwrite = TRUE)
+usethis::use_data(summary_lowrel, overwrite = TRUE)
